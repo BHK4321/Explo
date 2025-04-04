44e1f099115163811c5c7c85a72d50cbc830c105
Bhaskar Pandey1 <bhaskarpandey787@gmail.com>
2025-04-04 18:44:33 +0530

    ok

---
+++ config.example.h
@@ -0,0 +1,9 @@
+// config.example.h
+#ifndef CONFIG_H
+#define CONFIG_H
+
+const char* ssid = "YOUR_WIFI_SSID";
+const char* password = "YOUR_WIFI_PASSWORD";
+const char* mqtt_server = "YOUR_MQTT_BROKER_IP";
+
+#endif
